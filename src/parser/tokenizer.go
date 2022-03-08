e4e1159b5407327cedaf4e1f91228add3dece63e
Hauke Stieler <mail@hauke-stieler.de>
2022-03-09 00:39:47 +0100

    Re-code the bold/italic parsing

--- src/parser/tokenizer.go
+++ src/parser/tokenizer.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"github.com/hauke96/sigolo"
 	"github.com/hauke96/wiki2book/src/util"
 	"path/filepath"
 	"regexp"
@@ -198,50 +199,196 @@ func (t *Tokenizer) parseHeadings(content string) string {
 	return content
 }
 
+type BoldItalicStackItemType int
+
+const (
+	BOLD_OPEN BoldItalicStackItemType = iota
+	BOLD_CLOSE
+	ITALIC_OPEN
+	ITALIC_CLOSE
+)
+
+type BoldItalicStackItem struct {
+	itemType BoldItalicStackItemType
+	index    int
+	length   int
+}
+
 func (t *Tokenizer) parseBoldAndItalic(content string) string {
-	content, _, _, _ = t.tokenizeBoldAndItalic(content, 0, false, false)
+	var success bool
+	index := 0
+
+	// The idea: Search for the first  '  character in the content, this means a new bold or italic block might start.
+	// This block then gets parsed and we create the according markers. Then we search for the next block. This is
+	// based on the assumption that only a small percentage of the content is actually part of an italic or bold block.
+	// Because this approach is based on recursion, this per-block parsing reduces the recursion depth.
+	for {
+		for index < len(content) && content[index] != '\'' {
+			index++
+		}
+
+		if index >= len(content) {
+			break
+		}
+
+		var stack []BoldItalicStackItem
+		success, stack = t.tokenizeBoldAndItalic(content, index, false, false, stack)
+		if !success {
+			sigolo.Error("Unable to parse bold and italic tags in: %s", util.TruncString(content))
+			return content
+		}
+
+		// the index increase when inserting the markers as the content gets longer
+		offset := 0
+		chars := []byte(content)
+		for _, item := range stack {
+			itemIndex := item.index + offset
+			lenBefore := len(chars)
+			charsBefore := chars[:itemIndex]
+			var charsAfter []byte
+
+			switch item.itemType {
+			case BOLD_OPEN:
+				charsAfter = append([]byte(nil), chars[itemIndex+item.length:]...)
+				chars = append(charsBefore, []byte(MARKER_BOLD_OPEN)...)
+			case BOLD_CLOSE:
+				charsAfter = append([]byte(nil), chars[itemIndex+item.length:]...)
+				chars = append(charsBefore, []byte(MARKER_BOLD_CLOSE)...)
+			case ITALIC_OPEN:
+				charsAfter = append([]byte(nil), chars[itemIndex+item.length:]...)
+				chars = append(charsBefore, []byte(MARKER_ITALIC_OPEN)...)
+			case ITALIC_CLOSE:
+				charsAfter = append([]byte(nil), chars[itemIndex+item.length:]...)
+				chars = append(charsBefore, []byte(MARKER_ITALIC_CLOSE)...)
+			}
+
+			index = len(chars)
+			chars = append(chars, charsAfter...)
+			offset += len(chars) - lenBefore
+		}
+
+		content = string(chars)
+	}
+
 	return content
 }
 
-func (t *Tokenizer) tokenizeBoldAndItalic(content string, index int, isBoldOpen bool, isItalicOpen bool) (string, int, bool, bool) {
+// tokenizeBoldAndItalic takes the content, an index in that content, if this index is currently in a bold or italic
+// block and a stack to parse the content for bold and italic blocks. It returns a success-bool and the resulting stack.
+func (t *Tokenizer) tokenizeBoldAndItalic(content string, index int, isBoldOpen bool, isItalicOpen bool, stack []BoldItalicStackItem) (bool, []BoldItalicStackItem) {
+	// The idea of this approach:
+	// Find the next possible match for a start/stop of an italic/bold block. Put that item on the stack (e.g.
+	// BOLD_START), increase the index and recursively move on to further parse the content. Whenever something's
+	// "flacky", stop using the new stack and proceed with the old one which has one item less.
+	// Example of such an exit-condition: Say  ''  have been interpreted as ITALIC_OPEN but one  '  is left -> this
+	// should probably a BOLD_OPEN or BOLD_CLOSE as there are three  '''. Move one step back, interpret the  '''  as a
+	// BOLD-block and move on from there.
+
 	for index < len(content) {
-		// In case of last opened italic marker
-		if index+3 <= len(content) && content[index:index+3] == "'''" {
-			if !isBoldOpen {
-				// -3 +3 to replace the ''' as well
-				content = strings.Replace(content, content[index:index+3], MARKER_BOLD_OPEN, 1)
-				index = index + len(MARKER_BOLD_OPEN)
+		nextTokenMightBeItalic := index+2 <= len(content) && content[index:index+2] == "''"
+		nextTokenMightBeBold := index+3 <= len(content) && content[index:index+3] == "'''"
+		mightBelongToPrevToken := index > 0 && content[index-1:index+1] == "''"
 
-				// Check for further nested italic tags
-				content, index, isBoldOpen, isItalicOpen = t.tokenizeBoldAndItalic(content, index, true, isItalicOpen)
-			} else {
-				// +3 to replace the '''
-				content = strings.Replace(content, content[index:index+3], MARKER_BOLD_CLOSE, 1)
-
-				// -3 because of the ''' we replaced above
-				return content, index + len(MARKER_BOLD_CLOSE), false, isItalicOpen
+		if nextTokenMightBeItalic {
+			itemType := ITALIC_OPEN
+			if isItalicOpen {
+				itemType = ITALIC_CLOSE
 			}
-		} else if index+2 <= len(content) && content[index:index+2] == "''" {
-			if !isItalicOpen {
-				// +2 to replace the ''
-				content = strings.Replace(content, content[index:index+2], MARKER_ITALIC_OPEN, 1)
-				index = index + len(MARKER_ITALIC_OPEN)
 
-				// Check for further nested italic tags
-				content, index, isBoldOpen, isItalicOpen = t.tokenizeBoldAndItalic(content, index, isBoldOpen, true)
-			} else {
-				// +2 to replace the ''
-				content = strings.Replace(content, content[index:index+2], MARKER_ITALIC_CLOSE, 1)
+			newStack := append(stack, BoldItalicStackItem{itemType: itemType, index: index, length: 2})
 
-				// -2 because of the '' we replaced above
-				return content, index + len(MARKER_ITALIC_CLOSE), isBoldOpen, false
+			// Crossover = The combination of opening bold followed by closing italic is invalid in most markup languages
+			// -> insert dummy-items to resolve this issue
+			hasCrossover := len(stack) > 0 && stack[len(stack)-1].itemType == BOLD_OPEN && itemType == ITALIC_CLOSE
+			if hasCrossover {
+				newStack = append(stack, BoldItalicStackItem{itemType: BOLD_CLOSE, index: index, length: 0})
+				newStack = append(newStack, BoldItalicStackItem{itemType: itemType, index: index, length: 2})
+				newStack = append(newStack, BoldItalicStackItem{itemType: BOLD_OPEN, index: index + 2, length: 0})
 			}
-		} else {
-			index++
+
+			success, newStack := t.tokenizeBoldAndItalic(content, index+2, isBoldOpen, !isItalicOpen, newStack)
+
+			if success {
+				// path went well -> end recursion and return
+				return true, newStack
+			}
+			// don't abort on crossover, maybe a bold token would work
+
+			// path went not well -> use old stack and try to match it with a bold item
+		}
+		if nextTokenMightBeBold {
+			itemType := BOLD_OPEN
+			if isBoldOpen {
+				itemType = BOLD_CLOSE
+			}
+
+			newStack := append(stack, BoldItalicStackItem{itemType: itemType, index: index, length: 3})
+
+			// Crossover = The combination of opening italic followed by closing bold is invalid in most markup languages
+			// -> insert dummy-items to resolve this issue
+			hasCrossover := len(stack) > 0 && stack[len(stack)-1].itemType == ITALIC_OPEN && itemType == BOLD_CLOSE
+			if hasCrossover {
+				newStack = append(stack, BoldItalicStackItem{itemType: ITALIC_CLOSE, index: index, length: 0})
+				newStack = append(newStack, BoldItalicStackItem{itemType: itemType, index: index, length: 3})
+				newStack = append(newStack, BoldItalicStackItem{itemType: ITALIC_OPEN, index: index + 3, length: 0})
+			}
+
+			success, newStack := t.tokenizeBoldAndItalic(content, index+3, !isBoldOpen, isItalicOpen, newStack)
+
+			if success {
+				// path went well -> end recursion and return
+				return true, newStack
+			}
+
+			// path went also not well -> tried italic *and* bold = all possibilities failed -> abort
+			return false, stack
+		}
+
+		if mightBelongToPrevToken {
+			// The current character belongs to a italic/bold token but did not match the above -> wrong token chosen in
+			//previous call (e.g. an italic token was chosen when there were ''' but instead a bold token makes more sense)
+			return false, stack
+		}
+
+		if nextTokenMightBeItalic {
+			// We're here because the next two quotes cannot be an italic token (otherwise we would've returned earlier)
+			// but they are also not part of a bold one -> something doesn't add up -> abort
+			return false, stack
+		}
+
+		// This is true when for all opening tags a closing tag was found and therefore no opening tags are left. This
+		// means all following characters (maybe even whole sentences or paragraphs) are not relevant for us. So we
+		// abort here.
+		closedBlock := len(stack) > 0 && hasOddNumberOfItems(stack)
+		if closedBlock {
+			break
+		}
+
+		index++
+	}
+
+	return hasOddNumberOfItems(stack), stack
+}
+
+// hasOddNumberOfItems determines if e.g. there are more opening bold tags than closing ones (-> number of bold tags is odd).
+func hasOddNumberOfItems(stack []BoldItalicStackItem) bool {
+	numberOfItalics := 0
+	numberOfBolds := 0
+
+	for _, item := range stack {
+		switch item.itemType {
+		case BOLD_OPEN:
+			numberOfBolds++
+		case BOLD_CLOSE:
+			numberOfBolds--
+		case ITALIC_OPEN:
+			numberOfItalics++
+		case ITALIC_CLOSE:
+			numberOfItalics--
 		}
 	}
 
-	return content, index, false, false
+	return numberOfItalics == 0 && numberOfBolds == 0
 }
 
 func (t *Tokenizer) parseGalleries(content string) string {
--- src/parser/tokenizer_test.go
+++ src/parser/tokenizer_test.go
@@ -57,32 +57,58 @@ func TestParseHeading(t *testing.T) {
 	}
 }
 
-func TestParseBoldAndItalic(t *testing.T) {
-	tokenizer := NewTokenizer("foo", "bar")
+func TestParseBoldAndItalic_wrongFormats(t *testing.T) {
+	var tokenizer Tokenizer
+	var content string
 
-	content := tokenizer.parseBoldAndItalic("''foo'' some text '''bar'''")
+	tokenizer = NewTokenizer("foo", "bar")
+	content = tokenizer.parseBoldAndItalic("'''a'''b''c")
+	test.AssertEqual(t, MARKER_BOLD_OPEN+"a"+MARKER_BOLD_CLOSE+"b''c", content)
+
+	tokenizer = NewTokenizer("foo", "bar")
+	content = tokenizer.parseBoldAndItalic("'''a''''b")
+	test.AssertEqual(t, "'''a''''b", content)
+
+	tokenizer = NewTokenizer("foo", "bar")
+	content = tokenizer.parseBoldAndItalic("'''a")
+	test.AssertEqual(t, "'''a", content)
+
+	tokenizer = NewTokenizer("foo", "bar")
+	content = tokenizer.parseBoldAndItalic("It's a beautiful day")
+	test.AssertEqual(t, "It's a beautiful day", content)
+}
+
+func TestParseBoldAndItalic(t *testing.T) {
+	var tokenizer Tokenizer
+	var content string
+
+	tokenizer = NewTokenizer("foo", "bar")
+	content = tokenizer.parseBoldAndItalic("''foo'' some text '''bar'''")
 	test.AssertEqual(t, MARKER_ITALIC_OPEN+"foo"+MARKER_ITALIC_CLOSE+" some text "+MARKER_BOLD_OPEN+"bar"+MARKER_BOLD_CLOSE, content)
 
 	tokenizer = NewTokenizer("foo", "bar")
 	content = tokenizer.parseBoldAndItalic("'''''a'''b''")
-	test.AssertEqual(t, MARKER_BOLD_OPEN+MARKER_ITALIC_OPEN+"a"+MARKER_BOLD_CLOSE+"b"+MARKER_ITALIC_CLOSE, content)
+	test.AssertEqual(t, MARKER_ITALIC_OPEN+MARKER_BOLD_OPEN+"a"+MARKER_BOLD_CLOSE+"b"+MARKER_ITALIC_CLOSE, content)
 
 	tokenizer = NewTokenizer("foo", "bar")
 	content = tokenizer.parseBoldAndItalic("'''''a''b'''")
-	test.AssertEqual(t, MARKER_BOLD_OPEN+MARKER_ITALIC_OPEN+"a"+MARKER_ITALIC_CLOSE+"b"+MARKER_BOLD_CLOSE, content)
-
-	tokenizer = NewTokenizer("foo", "bar")
-	content = tokenizer.parseBoldAndItalic("'''a''b'''c''")
-	test.AssertEqual(t, MARKER_BOLD_OPEN+"a"+MARKER_ITALIC_OPEN+"b"+MARKER_BOLD_CLOSE+"c"+MARKER_ITALIC_CLOSE, content)
+	test.AssertEqual(t, MARKER_ITALIC_OPEN+MARKER_BOLD_OPEN+"a"+MARKER_BOLD_CLOSE+MARKER_ITALIC_CLOSE+MARKER_BOLD_OPEN+"b"+MARKER_BOLD_CLOSE, content)
 
 	tokenizer = NewTokenizer("foo", "bar")
 	content = tokenizer.parseBoldAndItalic("'''foo [[bar]] abc'''")
 	test.AssertEqual(t, MARKER_BOLD_OPEN+"foo [[bar]] abc"+MARKER_BOLD_CLOSE, content)
 
-	// TODO this should work (according to pandoc and wikipedia parser)
-	//tokenizer = NewTokenizer("foo", "bar")
-	//content = tokenizer.parseBoldAndItalic("'''''plane'''tary '''m'''ass '''o'''bject''")
-	//test.AssertEqual(t, MARKER_ITALIC_OPEN+MARKER_BOLD_OPEN+"plane"+MARKER_BOLD_CLOSE+"tary "+MARKER_BOLD_OPEN+"m"+MARKER_BOLD_CLOSE+"ass "+MARKER_BOLD_OPEN+"o"+MARKER_BOLD_CLOSE+"bject"+MARKER_ITALIC_CLOSE, content)
+	tokenizer = NewTokenizer("foo", "bar")
+	content = tokenizer.parseBoldAndItalic("a'''b''c'''d''e")
+	test.AssertEqual(t, "a"+MARKER_BOLD_OPEN+"b"+MARKER_ITALIC_OPEN+"c"+MARKER_ITALIC_CLOSE+MARKER_BOLD_CLOSE+MARKER_ITALIC_OPEN+"d"+MARKER_ITALIC_CLOSE+"e", content)
+
+	tokenizer = NewTokenizer("foo", "bar")
+	content = tokenizer.parseBoldAndItalic("a''b'''c''d'''e")
+	test.AssertEqual(t, "a"+MARKER_ITALIC_OPEN+"b"+MARKER_BOLD_OPEN+"c"+MARKER_BOLD_CLOSE+MARKER_ITALIC_CLOSE+MARKER_BOLD_OPEN+"d"+MARKER_BOLD_CLOSE+"e", content)
+
+	tokenizer = NewTokenizer("foo", "bar")
+	content = tokenizer.parseBoldAndItalic("'''''plane'''tary '''m'''ass '''o'''bject''")
+	test.AssertEqual(t, MARKER_ITALIC_OPEN+MARKER_BOLD_OPEN+"plane"+MARKER_BOLD_CLOSE+"tary "+MARKER_BOLD_OPEN+"m"+MARKER_BOLD_CLOSE+"ass "+MARKER_BOLD_OPEN+"o"+MARKER_BOLD_CLOSE+"bject"+MARKER_ITALIC_CLOSE, content)
 }
 
 func TestParseGalleries(t *testing.T) {
