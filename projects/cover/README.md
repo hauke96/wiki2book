f83c840b182f7fd3a144ff0b3f1ac2a2a5675a82
Hauke Stieler <mail@hauke-stieler.de>
2022-11-03 13:38:12 +0100

    Add parameters to cover rendering script and extend README

--- projects/cover/README.md
+++ projects/cover/README.md
@@ -1,15 +1,23 @@
-# Cover generator
+# Cover generation
 
 The `generate-cover.sh` script generates a `cover.png` file with the given logo on top.
 
-## Parameters
+# CLI usage
 
-`./generate-cover.sh <title> <pre> <post>`
+`./generate-cover.sh [-h|--help] [-f|--font <font>] [-l|--logo <logo-file>] <title> <pre> <post>`
 
-* `title`: The title to show
-* `pre`: Text before the title
-* `post`: Text after the title
+Optional parameter:
 
-## Example
+* Use `-h` or `--help` to show usage information
+* `-f` or `--font`: Specify the LaTeX font, for example `--font lmodern`. Default is `libertinus`.
+* `-l` or `--logo`: Specify the logo file on top of the title, for example `--logo ../../my-logo.jpg`. Default is `./logo`.
+* `-t` or `--template`: Specify the LaTeX file to be used, for example `--template ../../my-template.tex`. Default is `./cover.tex`.
 
-`./generate-cover.sh "Astronomy" "This is what Wikipedia writes about ..."
+Arguments:
+* `title`: The title to show.
+* `pre`: Text before the title.
+* `post`: Text after the title.
+
+# Example
+
+`./generate-cover.sh -f lmodern -l ../logo.png "Astronomy" "This is what Wikipedia writes about ..."
--- projects/cover/generate-cover.sh
+++ projects/cover/generate-cover.sh
@@ -1,20 +1,73 @@
 #!/bin/bash
 
 set -e
-set -x
 
+POSITIONAL_ARGS=()
+
+# Logo and template files will be copied, so we need defaults here
+LOGO="./logo.png"
+TEMPLATE="./cover.tex"
+
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		-h|--help)
+			echo "Please read the README.md file for usage information."
+			exit
+			;;
+		-f|--font)
+			FONT="$2"
+			shift # past argument
+			shift # past value
+			;;
+		-l|--logo)
+			LOGO=$2
+			shift # past argument
+			shift # past value
+			;;
+		-t|--template)
+			TEMPLATE=$2
+			shift # past argument
+			shift # past value
+			;;
+		-*|--*)
+			echo "Unknown option $1"
+			exit 1
+			;;
+		*)
+			POSITIONAL_ARGS+=("$1") # save positional arg
+			shift # past argument
+			;;
+	esac
+done
+
+LOGO_BASENAME=$(basename "$LOGO")
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+# Remove temp-folder from previous run and create new one
 rm -rf .tmp
 mkdir -p .tmp
 
-cp cover.tex logo.png .tmp
+# Copy necessary files
+cp "$TEMPLATE" .tmp/cover.tex
+cp "$LOGO" .tmp
+
 cd .tmp
 
+# Turn every \ into \\
 ESCAPED_TITLE=${1//\\/\\\\}
 ESCAPED_PRE=${2//\\/\\\\}
 ESCAPED_PST=${3//\\/\\\\}
+ESCAPED_FONT=${FONT//\\/\\\\}
+ESCAPED_LOGO=${LOGO_BASENAME//\\/\\\\}
+ESCAPED_LOGO=${ESCAPED_LOGO//\//\\/} # Turn / into \/ for sed
+
+# Replace the strings in the copy of the LaTeX document
 sed -i "s/((TITLE))/$ESCAPED_TITLE/" cover.tex
 sed -i "s/((PRE))/$ESCAPED_PRE/" cover.tex
 sed -i "s/((POST))/$ESCAPED_POST/" cover.tex
+sed -i "s/%((FONT))/\\\\usepackage{$ESCAPED_FONT}/" cover.tex
+sed -i "s/.\/logo/.\/$ESCAPED_LOGO/" cover.tex
 
 pdflatex ./cover.tex
 convert -background white -alpha remove cover.pdf cover.png
