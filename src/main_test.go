5d1c50660316658cd44cc264fbcc3f10916067e1
Hauke Stieler <mail@hauke-stieler.de>
2023-11-16 22:04:44 +0100

    Add test for profiling

---
+++ src/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/hauke96/sigolo"
+	"testing"
+	"wiki2book/config"
+)
+
+// This file is used to profile the application with the IntelliJ CPU profiler, which only works on test files.
+
+func TestArticleGermanLong(t *testing.T) {
+	err := config.LoadConfig("../configs/de.json")
+	sigolo.FatalCheck(err)
+
+	generateArticleEbook(
+		"Commodore 128",
+		"../.wiki2book/profiling.epub",
+		"../.wiki2book",
+		"",
+		"",
+		"../pandoc",
+		true,
+		true,
+	)
+}
