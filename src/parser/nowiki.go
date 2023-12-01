ae9098a94c1dbf9eab017137b5fca71ab9b8c90f
Hauke Stieler <mail@hauke-stieler.de>
2023-12-01 15:15:39 +0100

    Add nowiki parser files

---
+++ src/parser/nowiki.go
@@ -0,0 +1,36 @@
+package parser
+
+type NowikiToken struct {
+	Token
+	Content string
+}
+
+func (t *Tokenizer) parseNowiki(content string) string {
+	// The following steps are performed:
+	//   1. Split by the end token "</nowiki>" of nowiki areas
+	//   2. For each element in that slice, split by start token "<nowiki>" of comments
+	//   3. Only append the non-comment parts of the splits to the result segments
+
+	nowikiStart := "<nowiki>"
+	nowikiEnd := "</nowiki>"
+	nowikiStartLen := len(nowikiStart)
+	nowikiEndLen := len(nowikiEnd)
+
+	for i := 0; i < len(content)-nowikiEndLen; i++ {
+		cursor := content[i : i+nowikiStartLen]
+
+		if cursor == nowikiStart {
+			endIndex := findCorrespondingCloseToken(content, i+nowikiStartLen, nowikiStart, nowikiEnd)
+
+			token := NowikiToken{
+				Content: content[i+nowikiStartLen : endIndex],
+			}
+			tokenKey := t.getToken(TOKEN_NOWIKI)
+			t.setRawToken(tokenKey, token)
+
+			content = content[0:i] + tokenKey + content[endIndex+nowikiEndLen:]
+		}
+	}
+
+	return content
+}
---
+++ src/parser/nowiki_test.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"fmt"
+	"testing"
+	"wiki2book/test"
+)
+
+func TestNowiki(t *testing.T) {
+	tokenizer := NewTokenizer("foo", "bar")
+	content := "Foo<nowiki>something</nowiki> bar"
+	expectedContent := "Foo" + fmt.Sprintf(TOKEN_TEMPLATE, TOKEN_NOWIKI, 0) + " bar"
+
+	newContent := tokenizer.parseNowiki(content)
+
+	test.AssertEqual(t, expectedContent, newContent)
+	test.AssertMapEqual(t, map[string]Token{
+		fmt.Sprintf(TOKEN_TEMPLATE, TOKEN_NOWIKI, 0): NowikiToken{Content: "something"},
+	}, tokenizer.getTokenMap())
+}
+
+func TestNowiki_endOfText(t *testing.T) {
+	tokenizer := NewTokenizer("foo", "bar")
+	content := "Foo<nowiki>something</nowiki>"
+	expectedContent := "Foo" + fmt.Sprintf(TOKEN_TEMPLATE, TOKEN_NOWIKI, 0)
+
+	newContent := tokenizer.parseNowiki(content)
+
+	test.AssertEqual(t, expectedContent, newContent)
+	test.AssertMapEqual(t, map[string]Token{
+		fmt.Sprintf(TOKEN_TEMPLATE, TOKEN_NOWIKI, 0): NowikiToken{Content: "something"},
+	}, tokenizer.getTokenMap())
+}
