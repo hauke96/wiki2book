2a87649ab20497decbda5b58139379553f1bb737
Hauke Stieler <mail@hauke-stieler.de>
2025-02-23 14:16:03 +0100

    Clean up code and extract different parts into separate functions

--- src/parser/reference.go
+++ src/parser/reference.go
@@ -1,10 +1,8 @@
 package parser
 
 import (
-	"fmt"
 	"github.com/hauke96/sigolo/v2"
 	"strings"
-	"wiki2book/util"
 )
 
 type RefDefinitionToken struct {
@@ -54,109 +52,33 @@ func (t *Tokenizer) parseReferences(content string) string {
 
 	for i := 0; i < len(content)-refDefStartLen; i++ {
 		cursor := content[i : i+refDefStartLen]
+		if cursor != refDefStart && cursor != refPlaceholderEnd {
+			// Cursor is not on the beginning of any reference related tag.
+			continue
+		}
 
-		restOfContent := content[i:]
-		sigolo.Trace(restOfContent)
+		startEndIndex := findCorrespondingCloseToken(content, i+refDefStartLen, refDefStart, xmlClosing)
 
-		if cursor == refDefStart || cursor == refPlaceholderEnd {
-			startEndIndex := findCorrespondingCloseToken(content, i+refDefStartLen, refDefStart, xmlClosing)
+		if referencePlaceholderEndRegex.MatchString(content[i:startEndIndex+1]) || referencePlaceholderShortRegex.MatchString(content[i:startEndIndex+1]) {
+			// Tag like "</references>" or "<references />" found
+			content = t.parseReferenceEndPlaceholder(content, i, startEndIndex, refNumberCounter, refNumberToContent)
+			cursorWithinReferencePlaceholder = false
+		} else if referencePlaceholderStartRegex.MatchString(content[i : startEndIndex+1]) {
+			// Tag like "<references group=foo >" found
+			content = content[0:i] + content[startEndIndex+1:] // Remove tag from content
+			cursorWithinReferencePlaceholder = true
+		} else {
+			// Tag like "<ref name=..." or "<ref>..." found
+			nameAttributeValue := getNameAttribute(content[i+refDefStartLen : startEndIndex])
 
-			if referencePlaceholderEndRegex.MatchString(content[i:startEndIndex+1]) || referencePlaceholderShortRegex.MatchString(content[i:startEndIndex+1]) {
-				// Tag like "</references>" or "<references />" found
-
-				cursorWithinReferencePlaceholder = false
-
-				// Remove tag from content
-				contentBefore := strings.TrimRight(content[0:i], "\n") + "\n" // ensure this part ends with a newline
-				contentAfter := content[startEndIndex+1:]
-
-				// Generate list of references
-
-				for refNumber := 0; refNumber < refNumberCounter; refNumber++ {
-					if _, ok := refNumberToContent[refNumber]; !ok {
-						continue
-					}
-
-					tokenKey := t.getToken(TOKEN_REF_DEF)
-					t.setRawToken(tokenKey, RefDefinitionToken{
-						Index:   refNumber,
-						Content: refNumberToContent[refNumber],
-					})
-					contentBefore += tokenKey + "\n"
-
-					// Delete entry to prevent it from being used at the next placeholder again.
-					delete(refNumberToContent, refNumber)
-				}
-
-				content = strings.TrimRight(contentBefore, "\n") + contentAfter
-			} else if referencePlaceholderStartRegex.MatchString(content[i : startEndIndex+1]) {
-				// Tag like "<references group=foo >" found
-				cursorWithinReferencePlaceholder = true
-
-				// Remove tag from content
-				content = content[0:i] + content[startEndIndex+1:]
+			isReferenceUsage := content[startEndIndex-1] == '/' // Reference definitions end with "/>" instead of "</ref>"
+			if isReferenceUsage {
+				// Reference usage like "<ref name=foo />"
+				refNumberCounter, content = t.parseNamedReferenceUsage(content, i, nameAttributeValue, nameToRefNumber, refNumberCounter, cursorWithinReferencePlaceholder, startEndIndex)
 			} else {
-				// Tag like "<ref name=..." or "<ref>..." found
-				nameAttributeValue := getNameAttribute(content[i+refDefStartLen : startEndIndex])
-
-				isReferenceUsage := content[startEndIndex-1] == '/'
-				if isReferenceUsage {
-					if nameAttributeValue != "" {
-						// Names reference usage
-						refNumber, ok := nameToRefNumber[nameAttributeValue]
-						if !ok {
-							// Name appears the first time, the definition might come later
-							refNumber = refNumberCounter
-							nameToRefNumber[nameAttributeValue] = refNumber
-							refNumberCounter++
-						}
-
-						if !cursorWithinReferencePlaceholder {
-							tokenKey := t.getToken(TOKEN_REF_USAGE)
-							t.setRawToken(tokenKey, RefUsageToken{
-								Index: refNumber,
-							})
-							content = content[0:i] + tokenKey + content[startEndIndex+1:]
-						} else {
-							content = content[0:i] + content[startEndIndex+1:]
-						}
-					} else {
-						sigolo.Warnf("Named reference usage without name-attribute found: %s", content[i:startEndIndex])
-					}
-				} else {
-					// Reference definition like "<ref name=...>Foobar</ref".
-
-					refEndIndex := findCorrespondingCloseToken(content, startEndIndex, refDefStart, refDefLongEnd)
-
-					refNumber := refNumberCounter
-					if nameAttributeValue != "" {
-						if _, ok := nameToRefNumber[nameAttributeValue]; ok {
-							// Ref name already used before, so we use the number of this existing ref usage.
-							refNumber = nameToRefNumber[nameAttributeValue]
-						} else {
-							// Ref name appears for the first time, so we save the current counter value for later
-							// usages of this ref name.
-							nameToRefNumber[nameAttributeValue] = refNumberCounter
-						}
-					}
-
-					refNumberToContent[refNumber] = t.tokenizeContent(t, content[startEndIndex+1:refEndIndex])
-
-					if !cursorWithinReferencePlaceholder {
-						tokenKey := t.getToken(TOKEN_REF_USAGE)
-						t.setRawToken(tokenKey, RefUsageToken{
-							Index: refNumber,
-						})
-						content = content[0:i] + tokenKey + content[refEndIndex+refDefLongEndLen:]
-					} else {
-						content = content[0:i] + content[refEndIndex+refDefLongEndLen:]
-					}
-
-					if refNumber == refNumberCounter {
-						// We actually used the current count value, so we increase it for the next token.
-						refNumberCounter++
-					}
-				}
+				// Reference definition like "<ref name=...>Foobar</ref".
+				refEndIndex := findCorrespondingCloseToken(content, startEndIndex, refDefStart, refDefLongEnd)
+				refNumberCounter, content = t.parseReferenceDefinition(content, i, startEndIndex, refEndIndex, refNumberCounter, nameAttributeValue, nameToRefNumber, refNumberToContent, cursorWithinReferencePlaceholder, refDefLongEndLen)
 			}
 		}
 	}
@@ -164,164 +86,103 @@ func (t *Tokenizer) parseReferences(content string) string {
 	return content
 }
 
-func (t *Tokenizer) _parseReferences(content string) string {
-	// Split content in head, body and foot.
-	// Head is everything before the <references/> or </references> tag.
-	// Body is everything between <references> and </references>.
-	// Foot is everything after <references/> or </references>.
-	head, body, foot, noRefListFound := t.getReferenceHeadBodyFoot(content)
-	if noRefListFound {
-		return content
-	}
+// parseReferenceEndPlaceholder replaces the end of the given reference placeholder at index i, such as "<references />"
+// or "</references>" with a list of all references that occurred so far. It removes elements from the
+// refNumberToContent map. It returns the new content in which the reference end-token has been replaces by a newline-
+// separated list of reference definition token.
+func (t *Tokenizer) parseReferenceEndPlaceholder(content string, i int, startEndIndex int, refNumberCounter int, refNumberToContent map[int]string) string {
+	// Remove tag from content
+	contentBefore := strings.TrimRight(content[0:i], "\n") + "\n" // ensure this part ends with a newline
+	contentAfter := content[startEndIndex+1:]
 
-	refIndexToContent := map[int]string{}
-	nameToRefIndex := map[string]int{}
-	refIndexCounter := 0
+	// Generate list of references
 
-	refIndexCounter, newHead := t.parseStringForReferences(head, nameToRefIndex, refIndexToContent, refIndexCounter, true)
-	refIndexCounter, _ = t.parseStringForReferences(body, nameToRefIndex, refIndexToContent, refIndexCounter, false)
+	for refNumber := 0; refNumber < refNumberCounter; refNumber++ {
+		if _, ok := refNumberToContent[refNumber]; !ok {
+			continue
+		}
 
-	// Append ref definitions to head
-	for refIndex := 0; refIndex < refIndexCounter; refIndex++ {
 		tokenKey := t.getToken(TOKEN_REF_DEF)
 		t.setRawToken(tokenKey, RefDefinitionToken{
-			Index:   refIndex,
-			Content: t.tokenizeContent(t, refIndexToContent[refIndex]),
+			Index:   refNumber,
+			Content: refNumberToContent[refNumber],
 		})
-		newHead += tokenKey + "\n"
+		contentBefore += tokenKey + "\n"
+
+		// Delete entry to prevent it from being used at the next placeholder again.
+		delete(refNumberToContent, refNumber)
 	}
 
-	return newHead + foot
+	content = strings.TrimRight(contentBefore, "\n") + contentAfter
+	return content
 }
 
-// parseStringForReferences determines all reference usage and definition in the given text and stores that information
-// in the given maps. The appendTokenToContent flag determines whether tokens should be generated and appended
-// to the output string. The output int is the next (and not yet used ref index), the output string is the parsed
-// input with removed refs and optionally with token keys in it.
-func (t *Tokenizer) parseStringForReferences(stringToParse string, nameToRefIndex map[string]int, refIndexToContent map[int]string, refIndexCounter int, appendTokenToContent bool) (int, string) {
-	/*
-		Strategy:
+// parseNamedReferenceUsage replaces the occurrence of a named reference usage, such as "<ref name=foo />" at the given
+// index i with a reference usage token. It might increase the refNumberCounter, in case the reference appeared for the
+// first time, might change the nameToRefNumber map and returns the new content containing the key of the new reference
+// usage token.
+func (t *Tokenizer) parseNamedReferenceUsage(content string, i int, nameAttributeValue string, nameToRefNumber map[string]int, refNumberCounter int, cursorWithinReferencePlaceholder bool, startEndIndex int) (int, string) {
+	if nameAttributeValue != "" {
+		// Names reference usage
+		refNumber, ok := nameToRefNumber[nameAttributeValue]
+		if !ok {
+			// Name appears the first time, the definition might come later
+			refNumber = refNumberCounter
+			nameToRefNumber[nameAttributeValue] = refNumber
+			refNumberCounter++
+		}
 
-		Go through stringToParse and search for all occurrences of any reference stuff (usage, usage with name, usage
-		with definition). Then remember all the definitions and usages in their order to create correct numbering and
-		footnote entries later.
-
-		This is done by first splitting the content by "<ref" to obtain segments which all start with a reference. There
-		is some additional processing needed to only get relevant lines. The <reference/> tag also starts with <ref but
-		should not be considered. There might be more tags starting with "<ref" but having nothing to do with references.
-
-		Having all truly valid reference segments, they are parsed to obtain the name (<ref name="some name"...>) and the
-		content/body of the reference (<ref>some content</ref>).
-
-		So later join all segments back together into one string with tokens in it, the given maps and segmentToRefIndex
-		are used to store this information which reference with which index belongs to which segment.
-
-		In the end (if wanted, s. appendTokenToContent flag), tokens are created and the segments are joined back
-		together.
-
-		Some additional information:
-
-		Cases of reference definitions:
-		<ref>foo</ref>
-		<ref name="something">foo</ref>
-
-		Case of reference usage:
-		<ref name="something" />
-	*/
-
-	contentSegments := strings.Split(stringToParse, "<ref")
-
-	// Determine segments that actually start with a reference.
-	validContentSegments := []string{contentSegments[0]}
-	for i, segment := range contentSegments {
-		if i == 0 {
-			// First element is not a ref (it's in front of the first ref) and is already added above.
-			continue
-		} else if !strings.HasPrefix(segment, " ") && !strings.HasPrefix(segment, ">") {
-			// Segment starts with "<ref" but is not a reference -> merge with prior segment
-			validContentSegments[len(validContentSegments)-1] = validContentSegments[len(validContentSegments)-1] + "<ref" + segment
+		if !cursorWithinReferencePlaceholder {
+			tokenKey := t.getToken(TOKEN_REF_USAGE)
+			t.setRawToken(tokenKey, RefUsageToken{
+				Index: refNumber,
+			})
+			content = content[0:i] + tokenKey + content[startEndIndex+1:]
 		} else {
-			validContentSegments = append(validContentSegments, segment)
+			content = content[0:i] + content[startEndIndex+1:]
 		}
+	} else {
+		sigolo.Warnf("Named reference usage without name-attribute found: %s", content[i:startEndIndex])
 	}
+	return refNumberCounter, content
+}
 
-	contentSegments = validContentSegments
-
-	// This map stores the reference index per segment. A reference can be used multiple times, so an index might be
-	// used in multiple segments.
-	segmentToRefIndex := map[int]int{}
-
-	for i, segment := range contentSegments {
-		if i == 0 {
-			// First element of string.Split() doesn't contain a ref (it's located before the first ref)
-			continue
-		}
-
-		// Try to find normal ref definition
-		// 0 = everything before </ref> (so everything between <ref... and ...</ref>)
-		// 1 = everything after </ref>
-		segmentParts := strings.SplitN(segment, "</ref>", 2)
-
-		// <ref refAttributes>refContent</ref>contentAfterRef
-		var refAttributes string
-		var refContent string
-		var contentAfterRef string
-
-		if len(segmentParts) == 2 {
-			contentAfterRef = segmentParts[1]
-			split := strings.SplitN(segmentParts[0], ">", 2)
-			refAttributes = split[0]
-			refContent = split[1]
-		} else if len(segmentParts) < 2 {
-			// Found ref usage instead, so we have to split a bit differently
-			// 0 = everything before /> (so everything between <ref... and .../>)
-			// 1 = everything after />
-			segmentParts = strings.SplitN(segment, "/>", 2)
-			refAttributes = segmentParts[0]
-			contentAfterRef = segmentParts[1]
-		}
-
-		refName := getNameAttribute(refAttributes)
-		if refName == "" {
-			// Nameless ref found -> Create randomized ref name
-			refName = util.Hash(fmt.Sprintf("%d%s", i, segment))
-		}
-
-		// Store only the part without the </ref> and /> snippets. The element [1] contains the content after the ref,
-		// so it's easier for later to join all the segments back together into a new string with reference token in it.
-		contentSegments[i] = contentAfterRef
-
-		if existingRefIndex, ok := nameToRefIndex[refName]; ok {
-			// Ref with same refName was defined earlier, so we reuse the ref index counter value
-			segmentToRefIndex[i] = existingRefIndex
-			if refContent != "" {
-				refIndexToContent[existingRefIndex] = refContent
-			}
+// parseReferenceDefinition replaces the occurrence of a reference definition, such as "<ref>foo</ref>" at the given
+// index i which a reference usage token. The definition and its content is stored to the refNumberToContent map. In
+// case the reference definition has a name attribute, an entry is added to the nameToRefNumber. When the reference is
+// new, the refNumberCounter will be incremented and its new value returned. In case of an already known named reference,
+// this counter will not change and its current value will be returned.
+func (t *Tokenizer) parseReferenceDefinition(content string, i int, startEndIndex int, refEndIndex int, refNumberCounter int, nameAttributeValue string, nameToRefNumber map[string]int, refNumberToContent map[int]string, cursorWithinReferencePlaceholder bool, refDefLongEndLen int) (int, string) {
+	refNumber := refNumberCounter
+	if nameAttributeValue != "" {
+		if _, ok := nameToRefNumber[nameAttributeValue]; ok {
+			// Ref name already used before, so we use the number of this existing ref usage.
+			refNumber = nameToRefNumber[nameAttributeValue]
 		} else {
-			nameToRefIndex[refName] = refIndexCounter
-			segmentToRefIndex[i] = refIndexCounter
-			refIndexToContent[refIndexCounter] = refContent
-			refIndexCounter++
+			// Ref name appears for the first time, so we save the current counter value for later
+			// usages of this ref name.
+			nameToRefNumber[nameAttributeValue] = refNumberCounter
 		}
 	}
 
-	if !appendTokenToContent {
-		return refIndexCounter, ""
-	}
+	refNumberToContent[refNumber] = t.tokenizeContent(t, content[startEndIndex+1:refEndIndex])
 
-	// The first segment if before the first ref and can therefore be used unchanged.
-	newHead := contentSegments[0]
-
-	for i := 1; i <= len(segmentToRefIndex); i++ {
-		refIndex := segmentToRefIndex[i]
+	if !cursorWithinReferencePlaceholder {
 		tokenKey := t.getToken(TOKEN_REF_USAGE)
 		t.setRawToken(tokenKey, RefUsageToken{
-			Index: refIndex,
+			Index: refNumber,
 		})
-		newHead += tokenKey + contentSegments[i] // +1 because segment 0 was already added above
+		content = content[0:i] + tokenKey + content[refEndIndex+refDefLongEndLen:]
+	} else {
+		content = content[0:i] + content[refEndIndex+refDefLongEndLen:]
 	}
 
-	return refIndexCounter, newHead
+	if refNumber == refNumberCounter {
+		// We actually used the current count value, so we increase it for the next token.
+		refNumberCounter++
+	}
+
+	return refNumberCounter, content
 }
 
 // getNameAttribute determines the values after "name=" and supports quoted and unquoted attributes. When unquoted
@@ -354,33 +215,4 @@ func getNameAttribute(content string) string {
 
 	// No name found
 	return ""
-}
-
-// getReferenceHeadBodyFoot splits the content into section before, within and after the reference list tag. The return
-// values are head, body, foot and a boolean which is true if there's no reference list in content.
-func (t *Tokenizer) getReferenceHeadBodyFoot(content string) (string, string, string, bool) {
-	// No reference list found -> abort
-	if !referencePlaceholderStartRegex.MatchString(content) {
-		return "", "", "", true
-	}
-
-	contentParts := referencePlaceholderStartRegex.Split(content, -1)
-	// In case of dedicated <references>...</references> block
-	//   part 0 = head : everything before <references...>
-	//   part 1 = body : everything between <references> and </references>
-	//   part 2 = foot : everything after </references>
-	// In case of <references/>
-	//   part 0 = head: everything before <references/>
-	//   part 1 = foot: everything after <references/>
-	// Completely remove the reference section as we already parsed it above with the regex.
-	head := contentParts[0]
-	body := ""
-	foot := ""
-	if len(contentParts) == 2 {
-		foot = contentParts[1]
-	} else if len(contentParts) == 3 {
-		body = contentParts[1]
-		foot = contentParts[2]
-	}
-	return head, body, foot, false
 }
