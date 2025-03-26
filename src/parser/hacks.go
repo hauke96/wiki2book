d23325ab48da6b1f7fdd278d8a5c1c40afcd8bec
Hauke Stieler <mail@hauke-stieler.de>
2025-03-26 08:07:11 +0100

    Move cleanup functions into Tokenizer struct

--- src/parser/hacks.go
+++ src/parser/hacks.go
@@ -12,7 +12,7 @@ Some Wikpedia-specific stuff is just to weird or is language specific and has to
 // hackGermanRailwayTemplates takes the content and removed the combination "{{BS-table}} ... |}", because apparently
 // the "{{BS-table}}" template generates the head of a table, which simply is closed by a "|}". This is very specific to
 // this template, requires knowledge about its evaluation/use and is therefore considered a hack.
-func hackGermanRailwayTemplates(content string, startIndex int) (string, error) {
+func (t *Tokenizer) hackGermanRailwayTemplates(content string, startIndex int) (string, error) {
 	// TODO It can happen that a template looks like this: "{{template|args|}}" and the "|}" part confuses this hack function.
 	// This is because it thinks that "|}" is the end of a table, which it isn't in this case.
 
@@ -30,7 +30,7 @@ func hackGermanRailwayTemplates(content string, startIndex int) (string, error) 
 			// This nesting is a problem because "{{BS-table}}" and "{|" are both starting tokens and "|}" is the only
 			// end token. This cannot be handled by "findCorrespondingCloseToken()". Therefore, recursion is used here
 			// to ensure that no "{{BS-table}}" occurs after the current sliding window.
-			content, err = hackGermanRailwayTemplates(content, i+slidingWindowSize)
+			content, err = t.hackGermanRailwayTemplates(content, i+slidingWindowSize)
 			if err != nil {
 				return content, err
 			}
