43b642c1a312456090732252e3c6bec6cbec3694
Hauke Stieler <mail@hauke-stieler.de>
2026-09-04 14:08:30 +0200

    Add script to render math using MathJax

---
+++ math-renderer/index.mjs
@@ -0,0 +1,27 @@
+import MathJax from 'mathjax';
+import fs from 'node:fs';
+import process from 'node:process';
+
+await MathJax.init({
+    loader: {load: ['input/tex', 'output/svg']}
+});
+
+// Assumed param values:
+// 0 = Node runtime path
+// 1 = Path of this script
+// 2 = TeX content
+// 3 = Output file path
+const texString = process.argv[2];
+const outputFile = process.argv[3];
+
+const mathjaxContainerNode = await MathJax.tex2svgPromise(texString, {display: true});
+const svgNode = mathjaxContainerNode.children[0];
+const svgContent = MathJax.startup.adaptor.serializeXML(svgNode)
+
+fs.writeFile(outputFile, svgContent, err => {
+    if (err) {
+        console.error("Could not write to output file " + outputFile, err);
+    } else {
+        // file written successfully
+    }
+});
